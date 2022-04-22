82f86fc8864f8ccb8040af69516bfa4231420379
itIsV <soicinivt@gmail.com>
2022-04-22 18:00:26 -0300

    feat: Get sellers.

--- src/main/java/com/concat/projetointegrador/controller/sellerController/SellerController.java
+++ src/main/java/com/concat/projetointegrador/controller/sellerController/SellerController.java
@@ -3,13 +3,16 @@ package com.concat.projetointegrador.controller.sellerController;
 import com.concat.projetointegrador.model.sellerModel.Seller;
 import com.concat.projetointegrador.service.sellerService.SellerService;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -27,6 +30,15 @@ public class SellerController {
                 .toUri();
 
         return ResponseEntity.created(uri).body(newSeller);
+
+    }
+
+    @GetMapping("/api/sellers")
+        public ResponseEntity<List<Seller>> getSellers() {
+
+        List<Seller> sellers = sellerService.getSellers();
+
+        return ResponseEntity.ok(sellers);
 
     }
 
