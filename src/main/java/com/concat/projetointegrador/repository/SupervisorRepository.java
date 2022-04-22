886aa66db2e21061b9619ed32b02dd144492915c
renatograca <renatomacedo1995@gmail.com>
2022-04-22 18:48:31 -0300

    change type id

--- src/main/java/com/concat/projetointegrador/repository/SupervisorRepository.java
+++ src/main/java/com/concat/projetointegrador/repository/SupervisorRepository.java
@@ -3,5 +3,5 @@ package com.concat.projetointegrador.repository;
 import com.concat.projetointegrador.model.SupervisorModel;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
-public interface SupervisorRepository extends JpaRepositoryImplementation<SupervisorModel, String> {
+public interface SupervisorRepository extends JpaRepositoryImplementation<SupervisorModel, Long> {
 }
