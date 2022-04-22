5cc2f62fa6f456bca0a6d1313a98b2eefcd8d534
renatograca <renatomacedo1995@gmail.com>
2022-04-22 15:20:10 -0300

    Create SupervisorServiceTest and validLengthNameSupervisor

--- src/test/java/com/concat/projetointegrador/SupervisorServiceTest.java
+++ src/test/java/com/concat/projetointegrador/SupervisorServiceTest.java
@@ -1,11 +1,18 @@
 package com.concat.projetointegrador;
 
+import com.concat.projetointegrador.model.SupervisorModel;
+import com.concat.projetointegrador.service.SupervisorService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SupervisorServiceTest {
 
     @Test
     public void validLengthNameSupervisor() {
+        SupervisorService supervisorService = new SupervisorService();
 
+        SupervisorModel newSupervisor = supervisorService.create(supervisor);
+
+        Assertions.assertEquals(new SupervisorModel("super123", "Renato", "Graca"), newSupervisor);
     }
 }
