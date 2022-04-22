c3e82bc3c2d4cebfe7dd3e089b86fa2dd1289a61
renatograca <renatomacedo1995@gmail.com>
2022-04-22 15:27:12 -0300

    implement create supervisor

--- src/main/java/com/concat/projetointegrador/service/SupervisorService.java
+++ src/main/java/com/concat/projetointegrador/service/SupervisorService.java
@@ -1,4 +1,17 @@
 package com.concat.projetointegrador.service;
 
+import com.concat.projetointegrador.model.SupervisorModel;
+import com.concat.projetointegrador.repository.SupervisorRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SupervisorService {
+    private SupervisorRepository supervisorRepository;
+    public SupervisorModel create(SupervisorModel supervisor) {
+        return supervisorRepository.save(supervisor);
+    }
 }
