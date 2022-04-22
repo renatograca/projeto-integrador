e1e932eeea628ad00a85ee348dd00cfa47ae7c81
renatograca <renatomacedo1995@gmail.com>
2022-04-22 17:33:51 -0300

    add default supervisorRepository

--- src/main/java/com/concat/projetointegrador/service/SupervisorService.java
+++ src/main/java/com/concat/projetointegrador/service/SupervisorService.java
@@ -2,15 +2,17 @@ package com.concat.projetointegrador.service;
 
 import com.concat.projetointegrador.model.SupervisorModel;
 import com.concat.projetointegrador.repository.SupervisorRepository;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Service
 public class SupervisorService {
     private SupervisorRepository supervisorRepository;
+
+    public SupervisorService(SupervisorRepository supervisorRepository) {
+        super();
+        this.supervisorRepository = supervisorRepository;
+    }
+
     public SupervisorModel create(SupervisorModel supervisor) {
         if (supervisor.getName().length() < 3) throw new RuntimeException();
         return supervisorRepository.save(supervisor);
