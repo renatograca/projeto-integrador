a00e72fc00c003bf1703582e0f5ba118bf6a4402
renatograca <renatomacedo1995@gmail.com>
2022-04-22 15:53:24 -0300

    add lombok @Builder

--- src/main/java/com/concat/projetointegrador/model/SupervisorModel.java
+++ src/main/java/com/concat/projetointegrador/model/SupervisorModel.java
@@ -1,12 +1,14 @@
 package com.concat.projetointegrador.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Id;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SupervisorModel {
