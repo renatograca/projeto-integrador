16a480f4aaa21bf688ee9df297b921c848feb047
renatograca <renatomacedo1995@gmail.com>
2022-04-22 15:06:14 -0300

    Create SupervisorModel

---
+++ src/main/java/com/concat/projetointegrador/model/SupervisorModel.java
@@ -0,0 +1,10 @@
+package com.concat.projetointegrador.model;
+
+import javax.persistence.Id;
+
+public class SupervisorModel {
+    @Id
+    private String id;
+    private String name;
+    private String lastname;
+}
