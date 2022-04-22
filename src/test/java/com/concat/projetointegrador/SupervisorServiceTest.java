dec4ac6b487ab73cd2fa471a656adf17d69ac5b4
renatograca <renatomacedo1995@gmail.com>
2022-04-22 15:10:20 -0300

    Create SupervisorServiceTest

---
+++ src/test/java/com/concat/projetointegrador/SupervisorServiceTest.java
@@ -0,0 +1,11 @@
+package com.concat.projetointegrador;
+
+import org.junit.jupiter.api.Test;
+
+public class SupervisorServiceTest {
+
+    @Test
+    public void validLengthNameSupervisor() {
+
+    }
+}
