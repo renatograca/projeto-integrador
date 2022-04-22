b70fe0ebbd9464e3c894409aaa1a6d2bae64a283
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-04-22 17:59:53 -0300

    add SectorDTO

--- src/main/java/com/concat/projetointegrador/dto/SectorDTO.java
+++ src/main/java/com/concat/projetointegrador/dto/SectorDTO.java
@@ -3,14 +3,11 @@ package com.concat.projetointegrador.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class SectorDTO {
-
-    @NotBlank
-    private Long id;
-    @NotBlank
+    @NotNull
     private Integer capacity;
 }
