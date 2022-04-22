888119dc6da0d70b5f8d762a0780fb9931f715f9
renatograca <renatomacedo1995@gmail.com>
2022-04-22 18:48:00 -0300

    change type id

--- src/main/java/com/concat/projetointegrador/model/SupervisorModel.java
+++ src/main/java/com/concat/projetointegrador/model/SupervisorModel.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class SupervisorModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String lastname;
