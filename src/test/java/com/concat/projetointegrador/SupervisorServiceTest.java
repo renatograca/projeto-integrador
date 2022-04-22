8aff9877d44b426dbdcbd0664f38a894533dda7c
renatograca <renatomacedo1995@gmail.com>
2022-04-22 15:42:15 -0300

    change name test to createASupervisorWhenNameLengthBiggerThree and pass

--- src/test/java/com/concat/projetointegrador/SupervisorServiceTest.java
+++ src/test/java/com/concat/projetointegrador/SupervisorServiceTest.java
@@ -1,18 +1,28 @@
 package com.concat.projetointegrador;
 
 import com.concat.projetointegrador.model.SupervisorModel;
+import com.concat.projetointegrador.repository.SupervisorRepository;
 import com.concat.projetointegrador.service.SupervisorService;
+import net.bytebuddy.implementation.bind.annotation.Super;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 public class SupervisorServiceTest {
 
     @Test
-    public void validLengthNameSupervisor() {
-        SupervisorService supervisorService = new SupervisorService();
+    public void createASupervisorWhenNameLengthBiggerThree() {
+        SupervisorRepository supervisorRepository = Mockito.mock(SupervisorRepository.class);
+        SupervisorService supervisorService = new SupervisorService(supervisorRepository);
 
-        SupervisorModel newSupervisor = supervisorService.create(supervisor);
+        Mockito.when(supervisorRepository.save(Mockito.any(SupervisorModel.class))).thenReturn(createSupervisor());
+        SupervisorModel newSupervisor = supervisorService.create(createSupervisor());
 
-        Assertions.assertEquals(new SupervisorModel("super123", "Renato", "Graca"), newSupervisor);
+        Assertions.assertTrue(newSupervisor.getName().length() > 3);
+    }
+
+    private SupervisorModel createSupervisor() {
+        return new SupervisorModel("super123", "Renato", "Graca");
     }
 }
