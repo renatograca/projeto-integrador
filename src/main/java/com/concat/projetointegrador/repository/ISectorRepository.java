7e0d96c8939c52212cd37be14565fb4aeba63568
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-04-25 12:39:26 -0300

    add method findByIdAndActiveTrue

--- src/main/java/com/concat/projetointegrador/repository/ISectorRepository.java
+++ src/main/java/com/concat/projetointegrador/repository/ISectorRepository.java
@@ -4,6 +4,11 @@ import com.concat.projetointegrador.model.Sector;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ISectorRepository extends JpaRepository<Sector, Long> {
+    List<Sector> findAllByActiveTrue();
+    Optional<Sector> findByIdAndActiveTrue(Long id);
 }
