3333479a6372c7b8f08d8ae55e778786dc4e941f
jbarcelos <joao.barcelos@mercadolivre.com>
2022-04-25 11:41:11 -0300

    Adding delete method

--- src/main/java/com/concat/projetointegrador/controller/InboundOrderController.java
+++ src/main/java/com/concat/projetointegrador/controller/InboundOrderController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,7 +49,7 @@ public class InboundOrderController {
 		return ResponseEntity.ok(order);
     }
 	
-	@PutMapping("/{id}")
+	@DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id, @RequestBody InboundOrderDTO dto) {
     	service.delete(id);
 		return ResponseEntity.accepted().build();
