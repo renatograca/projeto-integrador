2e8c93d542d7f063ccda5978c24c73cf6aa409e5
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-04-25 18:00:37 -0300

    fix path at uri refactor delete response

--- src/main/java/com/concat/projetointegrador/controller/SectorController.java
+++ src/main/java/com/concat/projetointegrador/controller/SectorController.java
@@ -2,19 +2,16 @@ package com.concat.projetointegrador.controller;
 
 import com.concat.projetointegrador.dto.SectorDTO;
 import com.concat.projetointegrador.model.Sector;
-import com.concat.projetointegrador.repository.ISectorRepository;
 import com.concat.projetointegrador.service.SectorService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/sector")
@@ -30,13 +27,14 @@ public class SectorController {
     public ResponseEntity<Object> saveSector(@RequestBody @Valid SectorDTO sectorDTO, UriComponentsBuilder uriBuilder) {
         Sector sector = new Sector();
         BeanUtils.copyProperties(sectorDTO, sector);
+        service.save(sector);
 
         URI uri = uriBuilder
-                .path("/supervisor/{id}")
+                .path("/sector/{id}")
                 .buildAndExpand(sector.getId())
                 .toUri();
 
-        return ResponseEntity.created(uri).body(service.save(sector));
+        return ResponseEntity.created(uri).body(sector);
     }
 
     @GetMapping
@@ -54,18 +52,14 @@ public class SectorController {
             @PathVariable(value = "id") Long id,
             @RequestBody @Valid SectorDTO sectorDTO
     ) {
-        Optional<Sector> sectorOptional = service.findById(id);
-        if (sectorOptional.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sector not found! Either enter with another ID or create a new sector!");
-        }
-
-        Sector sector = sectorOptional.get();
+        Sector sector = service.findById(id);
         sector.setCapacity(sectorDTO.getCapacity());
         return ResponseEntity.status(HttpStatus.OK).body(service.save(sector));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> delete(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.delete(id));
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.accepted().build();
     }
 }
