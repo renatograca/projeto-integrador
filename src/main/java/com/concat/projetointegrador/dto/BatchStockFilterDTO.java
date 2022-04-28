b59b6a34ad6a33593603b11939ee9b72e1140f80
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-04-28 14:18:01 -0300

    add getter and setter to BatchStockFilterDTO

--- src/main/java/com/concat/projetointegrador/dto/BatchStockFilterDTO.java
+++ src/main/java/com/concat/projetointegrador/dto/BatchStockFilterDTO.java
@@ -2,9 +2,12 @@ package com.concat.projetointegrador.dto;
 
 import com.concat.projetointegrador.model.BatchStock;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter @Setter
 @AllArgsConstructor
 public class BatchStockFilterDTO {
 
