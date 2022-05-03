f748d9f94cebeaed2406ff5269148b3cf3e4842d
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-05-03 11:35:06 -0300

    add javadoc

--- src/main/java/com/concat/projetointegrador/service/SectorService.java
+++ src/main/java/com/concat/projetointegrador/service/SectorService.java
@@ -16,10 +16,21 @@ public class SectorService {
 
     private final SectorRepository repository;
 
+    /**
+     * Find a sector by its ID
+     * @param id Long - sectors ID
+     * @return Sector if it exists
+     * @throws EntityNotFound if sector doesn't exist
+     */
     public Sector findById(Long id) {
         return repository.findById(id).orElseThrow(() -> new EntityNotFound("Setor não encontrado! Tente outro ID ou crie um novo Setor!"));
     }
 
+    /**
+     * Find all sectors by its category
+     * @param category Category - the category of the sector
+     * @return a List of Sectors by its category
+     */
     public List<Sector> findByCategory(Category category) {
         return repository.findByCategory(category);
     }
