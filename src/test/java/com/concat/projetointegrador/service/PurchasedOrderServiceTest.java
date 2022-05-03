23228a85077e19d48cf2f2e11c20319a3b64f92f
itIsV <soicinivt@gmail.com>
2022-05-03 11:14:24 -0300

    start purchased order unit tests.

---
+++ src/test/java/com/concat/projetointegrador/service/PurchasedOrderServiceTest.java
@@ -0,0 +1,20 @@
+package com.concat.projetointegrador.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchasedOrderServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void update() {
+    }
+}
