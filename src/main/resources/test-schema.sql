b507d471efa3b20cbfe930861c218ca0661b13bd
Juliana27 <juliana.brito@mercadolivre.com>
2022-05-03 19:53:03 -0300

    criado testes de integração do purchase order controller

--- src/main/resources/test-schema.sql
+++ src/main/resources/test-schema.sql
@@ -6,6 +6,8 @@ insert into sector (id, capacity, supervisor_id, warehouse_id, category) values 
 
 insert into seller (id,name, last_name) values (1, 'Janette', 'Fearick');
 
+insert into buyer (id,name, last_name) values (1, 'Janette', 'Fearick');
+
 INSERT INTO product (id,category,name,price,volume,seller_id) VALUES
 (1,'CONGELADOS','frango',20.00,1,1);
 
@@ -13,4 +15,8 @@ insert into inbound_order (id, order_date, sector_id) values (1, '2022-04-20', 1
 
 insert into batch_stock (id, category, initial_quantity, current_quantity, initial_temperature, current_temperature, manufacturing_date,
 manufacturing_time, due_date, product_id, inbound_order_id) values (1, 'CONGELADOS', 10, 10, 9, 9, '2022-04-20', '20:00:00', '2023-10-10', 1, null);
+
+insert into purchased_order (id, date, status, buyer_id) values (1,'2022-04-20', 'aberto', 1);
+
+insert into cart (id, quantity, products_id, purchased_order_id) values (1, 10, 1, 1);
 
