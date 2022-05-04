c0a280ee1ae355918fe4a7665385ab5d5b440d79
Juliana27 <juliana.brito@mercadolivre.com>
2022-05-04 15:24:57 -0300

    realizado teste do metodo create no SupervisorService e acrescentado autenticação aos testes de integração

--- src/test/java/com/concat/projetointegrador/integration/PurchaseOrderControllerTest.java
+++ src/test/java/com/concat/projetointegrador/integration/PurchaseOrderControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,13 +29,14 @@ public class PurchaseOrderControllerTest {
     private MockMvc mockMvc;
 
     private String payload() {
-        return "{\"buyer\":{\"id\":1},\"status\":\"aberto\",\"cart\":[{\"products\":{\"id\": 1},\"quantity\": 5}]}";
+        return "{\"buyer\":{\"id\":4},\"status\":\"aberto\",\"cart\":[{\"products\":{\"id\": 1},\"quantity\": 5}]}";
     }
 
     @Test
     public void shouldCreateAPurchaseOrderAndReturn201() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/orders")
                 .contentType(MediaType.APPLICATION_JSON)
+                .with(user("Buyer").password("123"))
                 .content(payload()))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType("application/json"))
@@ -44,28 +46,24 @@ public class PurchaseOrderControllerTest {
     @Test
     public void shouldFindByIdAndReturn200() throws Exception {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/orders")
+                .with(user("Buyer").password("123"))
                 .param("id", "1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
         String response = result.getResponse().getContentAsString();
-        assertEquals("{\"id\":1,\"date\":\"2022-04-20\",\"status\":\"aberto\",\"buyer\":{\"id\":1,\"name\":" +
-                "\"Janette\",\"lastName\":\"Fearick\",\"cpf\":null},\"cart\":[{\"id\":1,\"quantity\":10,\"products\":" +
-                "{\"id\":1,\"name\":\"frango\",\"volume\":1,\"price\":20.00,\"category\":\"CONGELADOS\",\"seller\":" +
-                "{\"id\":1,\"name\":\"Janette\",\"lastName\":\"Fearick\"}}}]}", response);
+        assertEquals("{\"id\":1,\"date\":\"2022-04-20\",\"status\":\"aberto\",\"buyer\":{\"id\":1,\"username\":\"ADMIN\",\"password\":\"$2a$10$81VuVXFi5JmfOdCblgjj0ODqsn11TUXfUEYnm.jInkbUIcd9xx31u\",\"name\":\"ADMIN\",\"lastName\":\"ADMIN\",\"cpf\":null,\"enabled\":true,\"discriminatorValue\":\"Buyer\",\"accountNonExpired\":true,\"accountNonLocked\":true,\"credentialsNonExpired\":true,\"authorities\":[{\"authority\":\"Buyer\"}]},\"cart\":[{\"id\":1,\"quantity\":10,\"products\":{\"id\":1,\"name\":\"frango\",\"volume\":1,\"price\":20.00,\"category\":\"CONGELADOS\",\"seller\":{\"id\":3,\"username\":\"Seller\",\"password\":\"$2a$10$81VuVXFi5JmfOdCblgjj0ODqsn11TUXfUEYnm.jInkbUIcd9xx31u\",\"name\":\"Seller\",\"lastName\":\"Seller\",\"cpf\":null,\"enabled\":true,\"discriminatorValue\":\"Seller\",\"accountNonExpired\":true,\"accountNonLocked\":true,\"credentialsNonExpired\":true,\"authorities\":[{\"authority\":\"Seller\"}]}}}]}", response);
     }
 
     @Test
     public void shouldUpdateAndReturn200() throws Exception {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/orders")
+                .with(user("Buyer").password("123"))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(payload())
                 .param("id", "1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
         String response = result.getResponse().getContentAsString();
-        assertEquals("{\"id\":1,\"date\":\"2022-04-20\",\"status\":\"finalizado\",\"buyer\":{\"id\":1,\"name\":" +
-                "\"Janette\",\"lastName\":\"Fearick\",\"cpf\":null},\"cart\":[{\"id\":1,\"quantity\":10,\"products\":" +
-                "{\"id\":1,\"name\":\"frango\",\"volume\":1,\"price\":20.00,\"category\":\"CONGELADOS\",\"seller\":" +
-                "{\"id\":1,\"name\":\"Janette\",\"lastName\":\"Fearick\"}}}]}", response);
+        assertEquals("{\"id\":1,\"date\":\"2022-04-20\",\"status\":\"finalizado\",\"buyer\":{\"id\":1,\"username\":\"ADMIN\",\"password\":\"$2a$10$81VuVXFi5JmfOdCblgjj0ODqsn11TUXfUEYnm.jInkbUIcd9xx31u\",\"name\":\"ADMIN\",\"lastName\":\"ADMIN\",\"cpf\":null,\"enabled\":true,\"discriminatorValue\":\"Buyer\",\"accountNonExpired\":true,\"accountNonLocked\":true,\"credentialsNonExpired\":true,\"authorities\":[{\"authority\":\"Buyer\"}]},\"cart\":[{\"id\":1,\"quantity\":10,\"products\":{\"id\":1,\"name\":\"frango\",\"volume\":1,\"price\":20.00,\"category\":\"CONGELADOS\",\"seller\":{\"id\":3,\"username\":\"Seller\",\"password\":\"$2a$10$81VuVXFi5JmfOdCblgjj0ODqsn11TUXfUEYnm.jInkbUIcd9xx31u\",\"name\":\"Seller\",\"lastName\":\"Seller\",\"cpf\":null,\"enabled\":true,\"discriminatorValue\":\"Seller\",\"accountNonExpired\":true,\"accountNonLocked\":true,\"credentialsNonExpired\":true,\"authorities\":[{\"authority\":\"Seller\"}]}}}]}", response);
     }
 }
