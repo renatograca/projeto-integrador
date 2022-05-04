a61d86d6cbe9b1dc73eb72da140fe80e5f4338fb
itIsV <soicinivt@gmail.com>
2022-05-04 18:10:46 -0300

    merge with develop

--- src/main/java/com/concat/projetointegrador/controller/PurchaseOrderController.java
+++ src/main/java/com/concat/projetointegrador/controller/PurchaseOrderController.java
@@ -1,12 +1,18 @@
 package com.concat.projetointegrador.controller;
 
+import com.concat.projetointegrador.dto.CartsDTO;
+import com.concat.projetointegrador.dto.ProductDTO;
 import com.concat.projetointegrador.dto.PurchasedOrderDTO;
+import com.concat.projetointegrador.dto.PurchasedOrderResponseDTO;
 import com.concat.projetointegrador.model.PurchasedOrder;
 import com.concat.projetointegrador.service.PurchasedOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/orders")
@@ -45,8 +51,21 @@ public class PurchaseOrderController {
      * @return returns a purchase order with status "completed" and status code 200 ok
      */
     @PutMapping
-    public ResponseEntity<PurchasedOrder> update(@RequestParam Long id) {
-        return ResponseEntity.ok(purchaseOrderService.update(id));
+    public ResponseEntity<PurchasedOrderResponseDTO> update(@RequestParam Long id) {
+        PurchasedOrder updatedPurchasedOrder = purchaseOrderService.update(id);
+        List<CartsDTO> carts = updatedPurchasedOrder.getCart().stream()
+                .map(cart -> CartsDTO.builder()
+                        .id(cart.getId())
+                        .quantity(cart.getQuantity())
+                        .productDTO(ProductDTO.convertToProductDTO(cart.getProducts()))
+                        .build()).collect(Collectors.toList());
+        PurchasedOrderResponseDTO purchasedOrderResponseDTO = PurchasedOrderResponseDTO.builder()
+                .id(updatedPurchasedOrder.getId())
+                .date(updatedPurchasedOrder.getDate())
+                .status(updatedPurchasedOrder.getStatus())
+                .cartsDTO(carts)
+                .build();
+        return ResponseEntity.ok(purchasedOrderResponseDTO);
     }
 
 }
