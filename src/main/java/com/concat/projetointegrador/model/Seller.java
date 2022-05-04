1652c7cf74c1673e8125ce6fb98ab0b6766a8029
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-05-04 09:37:43 -0300

    add lombok annotations to Seller

--- src/main/java/com/concat/projetointegrador/model/Seller.java
+++ src/main/java/com/concat/projetointegrador/model/Seller.java
@@ -8,12 +8,13 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Setter
 @Getter
+@Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Seller {
 
     @Id
@@ -26,5 +27,4 @@ public class Seller {
     @NotNull(message = "lastName não pode ser null")
     @Size(min = 1, max = 20, message = "LastName deve possuir de 1 a 20 caracteres.")
     private String lastName;
-
 }
