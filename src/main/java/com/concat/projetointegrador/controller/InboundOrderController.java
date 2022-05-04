f5acae718232e47a5264044b22f12871edee2586
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-05-04 09:36:07 -0300

    remove unnecessary imports

--- src/main/java/com/concat/projetointegrador/controller/InboundOrderController.java
+++ src/main/java/com/concat/projetointegrador/controller/InboundOrderController.java
@@ -6,12 +6,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.concat.projetointegrador.model.Sector;
-import com.concat.projetointegrador.model.Warehouse;
 import com.concat.projetointegrador.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
