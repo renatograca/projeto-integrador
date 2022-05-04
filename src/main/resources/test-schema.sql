57da9123a1bdae948e4944b7cdd2fdcd81d47420
Geovanna da Silva Mendes <geovanna.gmendes@mercadolivre.com>
2022-05-04 12:48:47 -0300

    integration test: create test for get route

--- src/main/resources/test-schema.sql
+++ src/main/resources/test-schema.sql
@@ -1,10 +1,18 @@
-insert into warehouse (id, name, region) values (1, 'Tools', 'Doujia');
+insert into WAREHOUSE (id, name, region) values (1, 'Tools', 'Doujia');
 
-insert into supervisor (id, name, last_Name) values (1, 'Basile', 'Van Hesteren');
+INSERT INTO "client" (role, id, cpf, last_name, name, password, username) VALUES('ADMIN', 1, NULL, 'ADMIN', 'ADMIN', '$2a$10$81VuVXFi5JmfOdCblgjj0ODqsn11TUXfUEYnm.jInkbUIcd9xx31u', 'ADMIN');
 
-insert into sector (id, capacity, supervisor_id, warehouse_id, category) values (1, 1, 1, 1, 'CONGELADOS');
+INSERT INTO "client" (role, id, cpf, last_name, name, password, username) VALUES('Supervisor', 2, NULL, 'Supervisor', 'Supervisor', '$2a$10$81VuVXFi5JmfOdCblgjj0ODqsn11TUXfUEYnm.jInkbUIcd9xx31u', 'Supervisor');
 
-insert into seller (id,name, last_name) values (1, 'Janette', 'Fearick');
+INSERT INTO "client" (role, id, cpf, last_name, name, password, username) VALUES('Seller', 3, NULL, 'Seller', 'Seller', '$2a$10$81VuVXFi5JmfOdCblgjj0ODqsn11TUXfUEYnm.jInkbUIcd9xx31u', 'Seller');
 
-INSERT INTO product (category,name,price,volume,seller_id) VALUES
-    ('CONGELADOS','frango',20.00,1,1);
+INSERT INTO "client" (role, id, cpf, last_name, name, password, username) VALUES('Buyer', 4, NULL, 'Buyer', 'Buyer', '$2a$10$81VuVXFi5JmfOdCblgjj0ODqsn11TUXfUEYnm.jInkbUIcd9xx31u', 'Buyer');
+
+INSERT INTO sector (id, capacity, category, "supervisor_id", warehouse_id) VALUES(1, 1000, 'CONGELADOS', 2, 1);
+
+INSERT INTO inbound_order (id, order_date, sector_id) VALUES(99, '2022-05-04', 1);
+
+INSERT INTO product (id,category,name,price,volume,"seller_id") VALUES
+    (1,'CONGELADOS','frango',20.00,1,3);
+
+INSERT INTO batch_stock (id, category, current_quantity, current_temperature, due_date, initial_quantity, initial_temperature, manufacturing_date, manufacturing_time, inbound_order_id, product_id) VALUES(99, 'CONGELADOS', 1, 2, '2025-10-10', 1, 2, '2022-10-10', '20:20:20', 99, 1);
--- src/test/java/com/concat/projetointegrador/integration/WarehouseControllerTest.java
+++ src/test/java/com/concat/projetointegrador/integration/WarehouseControllerTest.java
@@ -1,74 +1,83 @@
 package com.concat.projetointegrador.integration;
 
 import com.concat.projetointegrador.dto.WarehouseDTO;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.concat.projetointegrador.repository.WarehouseRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Sql(value = {"/test-schema.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class WarehouseControllerTest {
 
     @Autowired
-    private static MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Autowired
     private WarehouseRepository warehouseRepository;
-
-    @BeforeEach
-    void authorized() throws Exception {
-        token = String.valueOf(mockMvc
-                .perform(MockMvcRequestBuilders.post("/login")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("username", "geovanna")
-                .param("password","123"))
-                .andDo(print()));
-    }
-    private static String token;
 
     private String getObject() {
         return "{\"name\":\"armazem frios\", \"region\":\"melhor regiao\"}";
     }
 
-    private void createWarehouse() throws Exception {
-        mockMvc
-                .perform(MockMvcRequestBuilders.post("/warehouse")
+    @Test
+    public void shouldCreateAWarehouseAndReturn201() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/warehouse")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(getObject()))
+                        .content(getObject())
+                        .with(user("Supervisor").password("123")))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType("application/json"))
-                .andReturn();
-    }
-
-    @Test
-    @Sql({"/test-schema.sql"})
-    public void shouldCreateAWarehouseAndReturn201() throws Exception {
-        MvcResult mvcResult = mockMvc
-                .perform(MockMvcRequestBuilders.post("/warehouse")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(getObject()))
-                .andExpect(status().isCreated())
-                .andExpect(content().contentType("aplication/json"))
                 .andReturn();
 
         String response = mvcResult.getResponse().getContentAsString();
         ObjectMapper objectMapper = new ObjectMapper();
         WarehouseDTO warehouseDTO = objectMapper.readValue(response, WarehouseDTO.class);
         warehouseRepository.deleteById(warehouseDTO.getId());
+    }
+
+    @Test
+    public void shouldReturnWarehouseByIdReturnStatus200() throws Exception {
+        MvcResult result = mockMvc.perform(get("/warehouse/{id}", 1)
+                        .with(
+                                user("Supervisor")
+                                        .password("123")
+                        )
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+
+        String jsonReturned = result.getResponse().getContentAsString();
+
+        WarehouseDTO warehouseDTO = objectMapper.readValue(jsonReturned, WarehouseDTO.class);
+
+        assertEquals("Tools", warehouseDTO.getName());
     }
 }
