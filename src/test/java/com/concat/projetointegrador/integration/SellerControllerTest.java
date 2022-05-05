4241a9300e260409b5cf7e4e0ccfc560ec690646
Juliana27 <juliana.brito@mercadolivre.com>
2022-05-05 11:33:44 -0300

    criado teste de integração

--- src/test/java/com/concat/projetointegrador/integration/SellerControllerTest.java
+++ src/test/java/com/concat/projetointegrador/integration/SellerControllerTest.java
@@ -2,6 +2,7 @@ package com.concat.projetointegrador.integration;
 
 import com.concat.projetointegrador.dto.InboundOrderDTO;
 import com.concat.projetointegrador.dto.SellerDTO;
+import com.concat.projetointegrador.dto.SellerResponseDTO;
 import com.concat.projetointegrador.model.InboundOrder;
 import com.concat.projetointegrador.model.Seller;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -36,63 +37,86 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @Sql(value = {"/test-schema.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class SellerControllerTest {
 
-		@Autowired
-		private MockMvc mock;
+    @Autowired
+    private MockMvc mock;
 
-		@Test
-		public void shouldCreateAInboundOrder() throws Exception {
-				String payload = "{\"name\":\"joao\",\"lastName\":\"Silva\",\"username\":\"sell\",\"password\":123}";
-				SimpleGrantedAuthority supervisor = new SimpleGrantedAuthority("Supervisor");
-				ArrayList<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
-				simpleGrantedAuthorities.add(supervisor);
+    @Test
+    public void shouldCreateAInboundOrder() throws Exception {
+        String payload = "{\"name\":\"joao\",\"lastName\":\"Silva\",\"username\":\"sell\",\"password\":123}";
+        SimpleGrantedAuthority supervisor = new SimpleGrantedAuthority("Supervisor");
+        ArrayList<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
+        simpleGrantedAuthorities.add(supervisor);
 
-				MvcResult result = mock.perform(post("/seller")
-												.contentType(MediaType.APPLICATION_JSON)
-												.content(payload)
-								)
-								.andExpect(MockMvcResultMatchers.status().isCreated())
-								.andReturn();
+        MvcResult result = mock.perform(post("/seller")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload)
+        )
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn();
 
-				ObjectMapper objectMapper = new ObjectMapper();
-				objectMapper.registerModule(new JavaTimeModule());
-				objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-				objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
 
-				String jsonReturned = result.getResponse().getContentAsString();
-				SellerDTO inboundOrder = objectMapper.readValue(jsonReturned, SellerDTO.class);
+        String jsonReturned = result.getResponse().getContentAsString();
+        SellerDTO inboundOrder = objectMapper.readValue(jsonReturned, SellerDTO.class);
 
-				assertEquals(5, inboundOrder.getId());
+        assertEquals(5, inboundOrder.getId());
 
-		}
+    }
 
-		@Test
-		public void shouldFindAInboundOrderById() throws Exception {
+    @Test
+    public void shouldFindAInboundOrderById() throws Exception {
 
-				SimpleGrantedAuthority supervisor = new SimpleGrantedAuthority("Supervisor");
-				ArrayList<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
-				simpleGrantedAuthorities.add(supervisor);
+        SimpleGrantedAuthority supervisor = new SimpleGrantedAuthority("Supervisor");
+        ArrayList<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
+        simpleGrantedAuthorities.add(supervisor);
 
-				MvcResult result = mock.perform(get("/seller/{id}", 3)
-												.with(
-																user("Supervisor")
-																.password("123")
-																.authorities(simpleGrantedAuthorities)
-												)
-								)
-								.andExpect(MockMvcResultMatchers.status().isOk())
-								.andReturn();
+        MvcResult result = mock.perform(get("/seller/{id}", 3)
+                .with(
+                        user("Supervisor")
+                                .password("123")
+                                .authorities(simpleGrantedAuthorities)
+                )
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
 
-				ObjectMapper objectMapper = new ObjectMapper();
-				objectMapper.registerModule(new JavaTimeModule());
-				objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-				objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
 
-				String jsonReturned = result.getResponse().getContentAsString();
-				InboundOrder inboundOrder = objectMapper.readValue(jsonReturned, InboundOrder.class);
+        String jsonReturned = result.getResponse().getContentAsString();
+        InboundOrder inboundOrder = objectMapper.readValue(jsonReturned, InboundOrder.class);
 
-				assertEquals(3, inboundOrder.getId());
+        assertEquals(3, inboundOrder.getId());
 
-		}
+    }
+
+    @Test
+    public void shouldFindAllProductsBySeller() throws Exception {
+        SimpleGrantedAuthority supervisor = new SimpleGrantedAuthority("Supervisor");
+        ArrayList<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
+        simpleGrantedAuthorities.add(supervisor);
+
+        MvcResult result = mock.perform(get("/seller/products/{id}", 3)
+                .with(user("Supervisor").password("123")
+                .authorities(simpleGrantedAuthorities)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+
+        String jsonReturned = result.getResponse().getContentAsString();
+        SellerResponseDTO sellerResponseDTO = objectMapper.readValue(jsonReturned, SellerResponseDTO.class);
+
+        assertEquals("Seller", sellerResponseDTO.getName());
+    }
 
 }
 
