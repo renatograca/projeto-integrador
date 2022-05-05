fdecbe1ed5de518466e2e091193a876cfd006ef7
Juliana27 <juliana.brito@mercadolivre.com>
2022-05-05 11:24:37 -0300

    criado testes unitarios

--- src/test/java/com/concat/projetointegrador/unit/SellerServiceTest.java
+++ src/test/java/com/concat/projetointegrador/unit/SellerServiceTest.java
@@ -1,16 +1,25 @@
 package com.concat.projetointegrador.unit;
 
 import com.concat.projetointegrador.exception.EntityNotFound;
+import com.concat.projetointegrador.exception.ProductsNotRegistrate;
+import com.concat.projetointegrador.model.Category;
+import com.concat.projetointegrador.model.Product;
 import com.concat.projetointegrador.model.Seller;
+import com.concat.projetointegrador.repository.ProductRepository;
 import com.concat.projetointegrador.repository.SellerRepository;
 import com.concat.projetointegrador.service.SellerService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SellerServiceTest {
 
@@ -20,27 +29,29 @@ public class SellerServiceTest {
     public static final String PASSWORD = "NANI!!";
     public static final String LAST_NAME = "?";
     private SellerService service;
-    private SellerRepository repository = Mockito.mock(SellerRepository.class);
+    private SellerRepository sellerRepository = Mockito.mock(SellerRepository.class);
+    private ProductRepository productRepository = Mockito.mock(ProductRepository.class);
     private Optional<Seller> optionalSeller;
     private Seller seller;
     private Seller objectSeller;
+    private List<Product> products;
 
     @BeforeEach
     private void setUp() {
-        service = new SellerService(repository);
+        service = new SellerService(sellerRepository, productRepository);
         startSeller();
     }
 
     @Test
     public void whenFindByIDThenReturnASeller() {
-        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(optionalSeller);
+        Mockito.when(sellerRepository.findById(Mockito.anyLong())).thenReturn(optionalSeller);
         Seller response = service.findByID(ID);
         Assertions.assertEquals(response.getClass(), seller.getClass());
     }
 
     @Test
     public void shouldReturnAnErrorWhenNotFoundASeller() {
-        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+        Mockito.when(sellerRepository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
         EntityNotFound entityNotFound = Assertions
                 .assertThrows(EntityNotFound.class, () -> service.findByID(Mockito.anyLong()));
 
@@ -49,7 +60,7 @@ public class SellerServiceTest {
 
     @Test
     public void whenCreateASellerThenReturnASeller() {
-        Mockito.when(repository.save(Mockito.any())).thenReturn(seller);
+        Mockito.when(sellerRepository.save(Mockito.any())).thenReturn(seller);
         Seller response = service.create(objectSeller);
         Assertions.assertEquals(response.getClass(), seller.getClass());
     }
@@ -58,5 +69,22 @@ public class SellerServiceTest {
         seller = Seller.builder().id(ID).username(USERNAME).password(PASSWORD).name(NAME).lastName(LAST_NAME).build();
         objectSeller = Seller.builder().username(USERNAME).password(PASSWORD).name(NAME).lastName(LAST_NAME).build();
         optionalSeller = Optional.of(Seller.builder().id(ID).username(USERNAME).password(PASSWORD).name(NAME).lastName(LAST_NAME).build());
+        products = Arrays.asList(Product.builder().id(1L).name("carne").seller(seller).category(Category.CONGELADOS).price(BigDecimal.valueOf(20)).volume(1).build(),
+                Product.builder().id(2L).name("frango").seller(seller).category(Category.CONGELADOS).price(BigDecimal.valueOf(10)).volume(1).build());
     }
+
+    @Test
+    public void shouldReturnProductsBySeller() {
+        Mockito.when(productRepository.findProductBySeller(Mockito.any())).thenReturn(products);
+        List<Product> response = service.findAllProductsBySeller(seller);
+        Assertions.assertEquals(products, response);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenProductsBySeller() {
+        Mockito.when(productRepository.findProductBySeller(Mockito.any())).thenReturn(Mockito.anyList());
+        Throwable exception = assertThrows(ProductsNotRegistrate.class, () -> service.findAllProductsBySeller(seller));
+        assertEquals("Não existe produtos cadastrados para esse vendedor!", exception.getMessage());
+    }
+
 }
