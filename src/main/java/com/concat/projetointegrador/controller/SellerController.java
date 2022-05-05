32899873429985db3f8a9f2606e6f12f7c8587df
Juliana27 <juliana.brito@mercadolivre.com>
2022-05-04 22:14:10 -0300

    iniciando requisito 6

--- src/main/java/com/concat/projetointegrador/controller/SellerController.java
+++ src/main/java/com/concat/projetointegrador/controller/SellerController.java
@@ -1,6 +1,7 @@
 package com.concat.projetointegrador.controller;
 
 import com.concat.projetointegrador.dto.SellerDTO;
+import com.concat.projetointegrador.dto.SellerResponseDTO;
 import com.concat.projetointegrador.model.Seller;
 import com.concat.projetointegrador.service.SellerService;
 import lombok.AllArgsConstructor;
@@ -10,38 +11,43 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class SellerController {
 
-		private final SellerService sellerService;
+    private final SellerService sellerService;
 
 
-		/**
-		 *
-		 * @param seller user info
-		 * @param uriComponentsBuilder
-		 * @return new seller
-		 */
-		@PostMapping("/seller")
-		public ResponseEntity<SellerDTO> create(@RequestBody @Valid Seller seller, UriComponentsBuilder uriComponentsBuilder) {
-				SellerDTO newSeller = SellerDTO.convertToSellerDTO(sellerService.create(seller));
-				URI uri = uriComponentsBuilder.path("/seller/{id}")
-								.buildAndExpand(newSeller.getId())
-								.toUri();
-				return ResponseEntity.created(uri).body(newSeller);
-		}
+    /**
+     * @param seller user info
+     * @param uriComponentsBuilder
+     * @return new seller
+     */
+    @PostMapping("/seller")
+    public ResponseEntity<SellerDTO> create(@RequestBody @Valid Seller seller, UriComponentsBuilder uriComponentsBuilder) {
+        SellerDTO newSeller = SellerDTO.convertToSellerDTO(sellerService.create(seller));
+        URI uri = uriComponentsBuilder.path("/seller/{id}")
+                .buildAndExpand(newSeller.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(newSeller);
+    }
 
 
-		/**
-		 *
-		 * @param id - long
-		 * @return seller
-		 */
-		@GetMapping("/seller/{id}")
-		public ResponseEntity<Seller> findByID(@PathVariable Long id) {
-				Seller seller = sellerService.findByID(id);
-				return ResponseEntity.ok(seller);
-		}
+    /**
+     * @param id - long
+     * @return seller
+     */
+    @GetMapping("/seller/{id}")
+    public ResponseEntity<Seller> findByID(@PathVariable Long id) {
+        Seller seller = sellerService.findByID(id);
+        return ResponseEntity.ok(seller);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<SellerResponseDTO> findAllProductsBySeller(@PathVariable Long id) {
+
+        return null;
+    }
 }
---
+++ src/main/java/com/concat/projetointegrador/dto/SellerResponseDTO.java
@@ -0,0 +1,13 @@
+package com.concat.projetointegrador.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SellerResponseDTO {
+
+
+}
