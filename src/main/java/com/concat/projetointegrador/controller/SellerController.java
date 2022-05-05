a150befe841e669470184744f9e16fa254f21065
Juliana27 <juliana.brito@mercadolivre.com>
2022-05-04 22:55:22 -0300

    alteração na sellerController, anotado requestMapping para a classe toda

--- src/main/java/com/concat/projetointegrador/controller/SellerController.java
+++ src/main/java/com/concat/projetointegrador/controller/SellerController.java
@@ -4,6 +4,7 @@ import com.concat.projetointegrador.dto.SellerDTO;
 import com.concat.projetointegrador.model.Seller;
 import com.concat.projetointegrador.service.SellerService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -12,10 +13,11 @@ import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
-@AllArgsConstructor
+@RequestMapping("/seller")
 public class SellerController {
 
-    private final SellerService sellerService;
+    @Autowired
+    private SellerService sellerService;
 
     /**
      * create a seller
@@ -23,7 +25,7 @@ public class SellerController {
      * @param uriComponentsBuilder
      * @return a new seller
      */
-    @PostMapping("/seller")
+    @PostMapping
     public ResponseEntity<SellerDTO> create(@RequestBody @Valid Seller seller, UriComponentsBuilder uriComponentsBuilder) {
         SellerDTO newSeller = SellerDTO.convertToSellerDTO(sellerService.create(seller));
         URI uri = uriComponentsBuilder.path("/seller/{id}")
@@ -37,7 +39,7 @@ public class SellerController {
      * @param id Long - seller id
      * @return a seller
      */
-    @GetMapping("/seller/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<SellerDTO> findByID(@PathVariable Long id) {
         SellerDTO seller = SellerDTO.convertToSellerDTO(sellerService.findByID(id));
         return ResponseEntity.ok(seller);
